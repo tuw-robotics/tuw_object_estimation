4ca7922f0380ee94a2c8cce47cdcf7df56a7193b
Felix König <felix0koenig@gmail.com>
2019-02-21 10:17:59 +0100

    Reconfigure added. Auto reinitialization and timeout functionality added.

--- tuw_feed_mpn/include/tuw_feed_mpn/tuw_feed_mpn_node.h
+++ tuw_feed_mpn/include/tuw_feed_mpn/tuw_feed_mpn_node.h
@@ -15,7 +15,11 @@
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/message_filter.h>
+#include <dynamic_reconfigure/server.h>
 #include <message_filters/subscriber.h>
+#include <tuw_feed_mpn/tuw_feed_mpnConfig.h>
+#include <tf/transform_listener.h>
+#include <visualization_msgs/MarkerArray.h>
 
 namespace tuw
 {
@@ -24,6 +28,14 @@ namespace tuw
   {
   public:
 
+    struct FeedingConfig {
+      double distance;
+      double delay;
+      double timeout;
+      int rate;
+      bool auto_reinitialize;
+    };
+
     /**
      *
      */
@@ -31,11 +43,20 @@ namespace tuw
 
     /**
      *
+     * Callback for Parameterserver
+     *
+     * @param config
+     * @param level
+     */
+    void callbackParameters(tuw_feed_mpn::tuw_feed_mpnConfig &config, uint32_t level);
+
+    /**
+     *
      * Get tracked objects and feed to mpn at the specified rate
      *
      * @param track
      */
-    void callbackObjectDetection(tuw_object_msgs::ObjectDetection &tracks);
+    void callbackObjectDetection(const tuw_object_msgs::ObjectDetection &tracks);
 
     /**
      *
@@ -49,7 +70,7 @@ namespace tuw
      * @param tracks
      * @return
      */
-    bool initAndCheck(tuw_object_msgs::ObjectDetection &tracks);
+    bool initAndCheck(const tuw_object_msgs::ObjectDetection &tracks);
 
     /**
      * Publishes the last known pose of the object.
@@ -57,7 +78,45 @@ namespace tuw
      */
     void publish();
 
+    /**
+     *
+     * @param poseStamped check if it is save to forward a goal to the mpn.
+     */
+    bool checkSaveDistance(geometry_msgs::PoseStampedPtr &poseStamped);
+
+    /**
+     * get the publisher rate
+     *
+     * @return
+     */
+    int getRate()
+    {
+      return config_.rate;
+    }
+
+    /**
+     * Used for visualization -> make message out of route the person has travelled
+     *
+     * @param poseStamped
+     */
+    void appendToRouteSegments(geometry_msgs::PoseStampedPtr &poseStamped);
+
   private:
+    //ros
+    ros::Subscriber sub_tracks_;
+    ros::Publisher pub_topic_;
+    ros::Publisher pub_track_points_;
+    ros::Publisher pub_track_lines_;
+    visualization_msgs::Marker track_points_;
+    visualization_msgs::Marker track_lines_;
+    ros::Time last_track_update_;
+    ros::Duration timeout_time_;
+
+    //reconfigure
+    dynamic_reconfigure::Server<tuw_feed_mpn::tuw_feed_mpnConfig> reconfigureServer_;
+    dynamic_reconfigure::Server<tuw_feed_mpn::tuw_feed_mpnConfig>::CallbackType reconfigureFnc_;
+
+    FeedingConfig config_;
 
     int INVALID_ID = -1;
     ros::NodeHandle nh_;
@@ -70,8 +129,8 @@ namespace tuw
 
     tf2_ros::TransformListener tf_listener_;
     tf2_ros::Buffer tf_buffer_;
+    tf::TransformListener tf1_listener_;
     geometry_msgs::PoseStampedPtr latest_pose_;
-    ros::Publisher pub_topic_;
     int rate_;
     //Pose
 
--- tuw_feed_mpn/src/tuw_feed_mpn_node.cpp
+++ tuw_feed_mpn/src/tuw_feed_mpn_node.cpp
@@ -11,65 +11,136 @@ FeedMpnNode::FeedMpnNode(ros::NodeHandle &nh) : nh_(nh), tf_buffer_(ros::Duratio
   latest_pose_ = nullptr;
   trackedId_ = -1.0;
 
-  nh_.param<int>("rate", rate_, 1);
-
   world_frame_ = std::string("map");
-  robot_frame_ = std::string("r0/laser0");
+  robot_frame_ = std::string("p3dx/laser_front_right");
   nav_goal_topic_ = std::string("nav_goal");
+  std::string person_route_ = std::string("person_route");
   pub_topic_ = nh_.advertise<geometry_msgs::PoseStamped>(nav_goal_topic_, 1);
+  pub_track_points_ = nh_.advertise<visualization_msgs::Marker>(std::string("track_points"), 10);
+  pub_track_lines_ = nh_.advertise<visualization_msgs::Marker>(std::string("track_lines"), 10);
+  config_.distance = 1.0;
+  config_.delay = 0;
+  config_.timeout = 10;
+  timeout_time_ = ros::Duration(config_.timeout);
+
+  sub_tracks_ = nh_.subscribe("/r0/tracked_people", 1, &FeedMpnNode::callbackObjectDetection, this);
+  reconfigureFnc_ = boost::bind(&FeedMpnNode::callbackParameters, this, _1, _2);
+  reconfigureServer_.setCallback(reconfigureFnc_);
+  triggered_ = true;
+  init_radius_ = 10.0;
 }
 
-void FeedMpnNode::callbackObjectDetection(tuw_object_msgs::ObjectDetection &tracks)
+void FeedMpnNode::callbackParameters(tuw_feed_mpn::tuw_feed_mpnConfig &config, uint32_t level)
 {
+  config_.distance = config.save_distance;
+  config_.rate = config.rate;
+  config_.timeout = config.timeout;
+  config_.auto_reinitialize = config.auto_reinitialize;
+  timeout_time_ = ros::Duration(config_.timeout);
+}
+
+void FeedMpnNode::appendToRouteSegments(geometry_msgs::PoseStampedPtr &poseStamped)
+{
+  if (track_points_.points.size() == 0)
+  {
+    track_points_.header = poseStamped->header;
+    track_points_.id = 0;
+    track_points_.type = visualization_msgs::Marker::POINTS;
+    track_points_.ns = "person_trajectory";
+    track_points_.scale.x = 0.1;
+    track_points_.scale.y = 0.1;
+    track_points_.color.g = 1;
+    track_points_.color.a = 1;
+
+    track_lines_.header = poseStamped->header;
+    track_lines_.id = 1;
+    track_lines_.ns = "person_trajectory";
+    track_lines_.scale.x = 0.1;
+    track_lines_.scale.y = 0.1;
+    track_lines_.type = visualization_msgs::Marker::LINE_STRIP;
+    track_lines_.color.b = 1.0;
+    track_lines_.color.a = 1.0;
+  }
+  track_points_.points.push_back(poseStamped->pose.position);
+  track_lines_.points.push_back(poseStamped->pose.position);
+}
+
+void FeedMpnNode::callbackObjectDetection(const tuw_object_msgs::ObjectDetection &tracks)
+{
+  ROS_INFO("getting tracks");
+  ros::Duration duration = ros::Time::now() - last_track_update_;
+  std::cout << "duration since last üpdate " << duration.sec << "s" << std::endl;
+  if (duration > timeout_time_)
+  {
+    ROS_INFO("timeout");
+    if (config_.auto_reinitialize)
+    {
+      triggered_ = true;
+      trackedId_ = INVALID_ID;
+      ROS_INFO("auto-reinitialize");
+    } else
+    {
+      ROS_INFO("auto-reinitialize deactivated: track is lost");
+      trackedId_ = INVALID_ID;
+    }
+  }
   if (!triggered_ && trackedId_ == INVALID_ID)
   {
+    ROS_INFO("not triggered therefore do nothing");
     return;
   } else if (trackedId_ != INVALID_ID) //initialized: do tracking
   {
-
+    ROS_INFO("find matching track and update pose");
     for (auto &&object : tracks.objects)
     {
       if (object.object.ids[0] == trackedId_)
       {
-        auto &pose_wf = object.object.pose;
+        auto &pose_rf = object.object.pose;
         latest_pose_.reset(new geometry_msgs::PoseStamped());
         latest_pose_->header.stamp = ros::Time::now();
         latest_pose_->header.frame_id = tracks.header.frame_id;
-        latest_pose_->pose = pose_wf;
+        latest_pose_->pose = pose_rf;
+        appendToRouteSegments(latest_pose_);
+        last_track_update_ = ros::Time::now();
       }
     }
-
   } else //first time: do initialization
   {
-    initAndCheck(tracks);
+    ROS_INFO("init sequence");
+    if (!initAndCheck(tracks))
+    {
+      ROS_INFO("init not sucessful, try standing not too close to the robot");
+    } else
+    {
+      last_track_update_ = ros::Time::now();
+    }
   }
 }
 
-bool FeedMpnNode::initAndCheck(tuw_object_msgs::ObjectDetection &tracks)
+bool FeedMpnNode::initAndCheck(const tuw_object_msgs::ObjectDetection &tracks)
 {
   std::vector<float> dists(tracks.objects.size());
-  uint32_t tidx=0;
-  uint32_t tidx_chosen=0;
+  uint32_t tidx = 0;
+  uint32_t tidx_chosen = 0;
   for (auto &&object : tracks.objects)
   {
-
-    auto &pose_wf = object.object.pose;
-    geometry_msgs::PoseStamped pose_wf_stamped;
-    pose_wf_stamped.pose = pose_wf;
-    pose_wf_stamped.header = tracks.header;
+    auto &pose_rf = object.object.pose;
+    geometry_msgs::PoseStamped pose_rf_stamped;
+    pose_rf_stamped.pose = pose_rf;
+    pose_rf_stamped.header = tracks.header;
 
     //geometry_msgs::TransformStamped stamped_tf = tf_buffer_.lookupTransform(
     //    world_frame_, robot_frame_, ros::Time( 0 ));
 
-    geometry_msgs::PointStamped point_wf;
-    point_wf.header = tracks.header;
-    point_wf.point = pose_wf.position;
-    geometry_msgs::PointStamped point_rf;
-    tf_buffer_.transform(point_wf, point_rf, robot_frame_);
+    //geometry_msgs::PointStamped point_wf;
+    //point_wf.header = tracks.header;
+    //point_wf.point = pose_wf.position;
+    //geometry_msgs::PointStamped point_rf;
+    //tf_buffer_.transform(point_wf, point_rf, robot_frame_);
 
-    double xx = point_rf.point.x * point_rf.point.x;
-    double yy = point_rf.point.y * point_rf.point.y;
-    double zz = point_rf.point.z * point_rf.point.z;
+    double xx = pose_rf_stamped.pose.position.x * pose_rf_stamped.pose.position.x;
+    double yy = pose_rf_stamped.pose.position.y * pose_rf_stamped.pose.position.y;
+    double zz = pose_rf_stamped.pose.position.z * pose_rf_stamped.pose.position.z;
     double euclidean = sqrt(xx + yy + zz);
 
     dists[tidx++] = euclidean;
@@ -77,15 +148,16 @@ bool FeedMpnNode::initAndCheck(tuw_object_msgs::ObjectDetection &tracks)
     {
       tidx_chosen = tidx - 1;
       trackedId_ = object.object.ids[0];
-      latest_pose_.reset(new geometry_msgs::PoseStamped(pose_wf_stamped));
-      break;
+      latest_pose_.reset(new geometry_msgs::PoseStamped(pose_rf_stamped));
+      geometry_msgs::PointStamped point_rf;
+      ROS_INFO("From %d tracks %d has been chosen", static_cast<int>(tracks.objects.size()),
+               static_cast<int>(trackedId_));
+      ROS_INFO("Distance to robot is: %lf", dists[tidx_chosen]);
+      return true;
     }
 
   }
-
-  ROS_INFO("From %d tracks %d has been chosen", tracks.objects.size(), trackedId_);
-  ROS_INFO("Distance to robot is: %d", dists[tidx_chosen]);
-  return true;
+  return false;
 }
 
 void FeedMpnNode::triggerFeedingProcess(std_msgs::Float64 &init_radius_trigger)
@@ -94,24 +166,39 @@ void FeedMpnNode::triggerFeedingProcess(std_msgs::Float64 &init_radius_trigger)
   triggered_ = true;
 }
 
+bool FeedMpnNode::checkSaveDistance(geometry_msgs::PoseStampedPtr &poseStamped)
+{
+  auto &position = poseStamped->pose.position;
+  double xx = position.x * position.x;
+  double yy = position.y * position.y;
+
+  return std::sqrt(xx + yy) > config_.distance;
+}
+
 void FeedMpnNode::publish()
 {
-  if (latest_pose_)
+  if (latest_pose_ && checkSaveDistance(latest_pose_))
   {
     pub_topic_.publish(latest_pose_);
+    pub_track_points_.publish(track_points_);
+    pub_track_lines_.publish(track_lines_);
   }
 }
 
 int main(int argc, char **argv)
 {
+  ros::init(argc, argv, "tuw_feed_mpn_node");
+
   ros::NodeHandle nh;
-
-
   FeedMpnNode feeder(nh);
   ros::Rate r(feeder.getRate());
 
   while (ros::ok())
   {
+    //rate can change via reconfigure
+    r = ros::Rate(feeder.getRate());
     feeder.publish();
+    r.sleep();
+    ros::spinOnce();
   }
 }
